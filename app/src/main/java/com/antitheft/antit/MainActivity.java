0c62bc9492c02705f4990a8cdb6b03ce529595d3
milindc97 <milindchaudhari97@gmail.com>
2016-09-19 14:30:02 +0530

    Splash Screen Project
    This is an Splash Screen Project will help you to add splash screen in
    your android app. This is an Open source project. Splash Screen will run
    when application run on first time and then app redirect to Main
    Activity.

---
+++ app/src/main/java/com/antitheft/antit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.antitheft.antit;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * Created by Milind on 19/09/2016.
+ */
+public class MainActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
